477e6419abade9169f66ddeaf4bdf3092a60f30d
wangyuanchi <yuanchiri@gmail.com>
2025-06-12 22:34:14 +0800

    set up prisma

--- pipeline/main.go
+++ pipeline/main.go
@@ -63,7 +63,7 @@ func main() {
 		This performs the data migration to the "gems" table
 		An additional "backup" table is also manually created
 	*/
-	legacy.Migrate(ctx, pgq, "./pgdb/legacy/gems.json", false)
+	legacy.Migrate(ctx, pgq, "./pgdb/legacy/gems.json", true)
 
 	// Main task of this pipeline
 	sigCh := make(chan os.Signal, 1)
