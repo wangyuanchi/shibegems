26981d43d91536649158c9f44a504d5fa7511baf
wangyuanchi <yuanchiri@gmail.com>
2025-06-13 20:01:02 +0800

    beautify embed

--- bot/src/commands/stats.ts
+++ bot/src/commands/stats.ts
@@ -1,4 +1,8 @@
-import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import {
+  ChatInputCommandInteraction,
+  EmbedBuilder,
+  SlashCommandBuilder,
+} from "discord.js";
 
 import { getPrismaClient } from "../prisma";
 
@@ -21,7 +25,23 @@ async function execute(interaction: ChatInputCommandInteraction) {
       await interaction.reply("You do not have any gems.");
     } else {
       const { user_id, guild_id, ...gems } = row;
-      await interaction.reply(JSON.stringify(gems));
+
+      const embed = new EmbedBuilder()
+        .setColor(0x7289da)
+        .setTitle(`${interaction.user.username}'s Stats`)
+        .setThumbnail(interaction.user.displayAvatarURL())
+        .setFooter({ text: interaction.guild?.name! })
+        .setTimestamp();
+
+      for (const [key, value] of Object.entries(gems)) {
+        embed.addFields({
+          name: key.charAt(0).toUpperCase() + key.slice(1),
+          value: value.toString(),
+          inline: true,
+        });
+      }
+
+      await interaction.reply({ embeds: [embed] });
     }
   } catch (err) {
     console.error(err);
