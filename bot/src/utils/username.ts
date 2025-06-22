c91cf46d3628a2e374faa27930c167f09732b2ae
wangyuanchi <yuanchiri@gmail.com>
2025-06-22 20:45:15 +0800

    fix username bug

--- bot/src/utils/username.ts
+++ bot/src/utils/username.ts
@@ -6,12 +6,12 @@ export async function fetchDiscordUsername(user_id: bigint): Promise<string> {
   if (!username) {
     try {
       username = (await getDiscordClient().users.fetch(userID)).username;
-      if (username.startsWith("deleted_user_")) {
-        username = "Deleted User";
-      }
     } catch {
       username = userID;
     }
+  }
+  if (username.startsWith("deleted_user_")) {
+    username = "Deleted User";
   }
   return username;
 }
